f96e900c2ff4ebe51c511390ba0d9537bb70f762
Dean Gardiner <me@dgardiner.net>
2017-04-03 23:55:43 +1200

    uboot: renamed properties to match plugin name

--- fm_uboot.py
+++ fm_uboot.py
@@ -11,20 +11,20 @@ class UBootPlugin(KBuildPlugin):
     def schema(cls):
         schema = super().schema()
 
-        schema['properties']['x-uboot-board'] = {
+        schema['properties']['fm-uboot-board'] = {
             'type': 'string'
         }
 
-        schema['properties']['x-uboot-configfile'] = {
+        schema['properties']['fm-uboot-configfile'] = {
             'type': 'string'
         }
 
-        schema['properties']['x-uboot-image'] = {
+        schema['properties']['fm-uboot-image'] = {
             'type': 'string'
         }
 
         schema['required'] = schema['required'] + [
-            'x-uboot-image'
+            'fm-uboot-image'
         ]
 
         return schema
@@ -32,8 +32,8 @@ class UBootPlugin(KBuildPlugin):
     @classmethod
     def get_build_properties(cls):
         return super().get_build_properties() + [
-            'x-uboot-board',
-            'x-uboot-configfile'
+            'fm-uboot-board',
+            'fm-uboot-configfile'
         ]
 
     def enable_cross_compilation(self):
@@ -52,27 +52,27 @@ class UBootPlugin(KBuildPlugin):
         if self.options.kconfigfile is not None:
             raise ValueError('"kconfigfile" option is not supported')
 
-        if not self.options.x_uboot_board:
-            raise ValueError('Invalid value provided for the "x-uboot-board" option')
+        if not self.options.fm_uboot_board:
+            raise ValueError('Invalid value provided for the "fm-uboot-board" option')
 
-        if not self.options.x_uboot_image:
-            raise ValueError('Invalid value provided for the "x-uboot-image" option')
+        if not self.options.fm_uboot_image:
+            raise ValueError('Invalid value provided for the "fm-uboot-image" option')
 
         # Run build
         super().build()
 
     def do_base_config(self, config_path):
-        kdefconfig = '{}_defconfig'.format(self.options.x_uboot_board)
+        kdefconfig = '{}_defconfig'.format(self.options.fm_uboot_board)
 
         # Copy configuration file into `configs/` (if provided)
-        if self.options.x_uboot_configfile:
-            if not os.path.exists(self.options.x_uboot_configfile):
-                raise ValueError('Unable to find configuration file: {}'.format(self.options.x_uboot_configfile))
+        if self.options.fm_uboot_configfile:
+            if not os.path.exists(self.options.fm_uboot_configfile):
+                raise ValueError('Unable to find configuration file: {}'.format(self.options.fm_uboot_configfile))
 
-            logger.debug('Copying "%s" to "configs/%s"...', self.options.x_uboot_configfile, kdefconfig)
+            logger.debug('Copying "%s" to "configs/%s"...', self.options.fm_uboot_configfile, kdefconfig)
 
             try:
-                shutil.copy(self.options.x_uboot_configfile, os.path.join(self.builddir, 'configs', kdefconfig))
+                shutil.copy(self.options.fm_uboot_configfile, os.path.join(self.builddir, 'configs', kdefconfig))
             except Exception as ex:
                 raise Exception('Unable to copy configuration file into configs/ directory: {}'.format(ex))
 
@@ -83,14 +83,14 @@ class UBootPlugin(KBuildPlugin):
         self.run(make_cmd + [kdefconfig])
 
     def do_install(self):
-        image_source_path = os.path.join(self.builddir, self.options.x_uboot_image)
+        image_source_path = os.path.join(self.builddir, self.options.fm_uboot_image)
 
         # Ensure u-boot image exists
         if not os.path.exists(image_source_path):
-            raise ValueError('U-Boot image does not exist: {}'.format(self.options.x_uboot_image))
+            raise ValueError('U-Boot image does not exist: {}'.format(self.options.fm_uboot_image))
 
         # Remove existing u-boot image
-        image_destination_path = os.path.join(self.installdir, self.options.x_uboot_image)
+        image_destination_path = os.path.join(self.installdir, self.options.fm_uboot_image)
 
         if os.path.exists(image_destination_path):
             os.remove(image_destination_path)
